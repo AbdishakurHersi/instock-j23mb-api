35904c8370cf46efc02c50d71f12881006609b92
GabeEllis <gabeellis830@gmail.com>
2023-03-22 16:44:33 -0400

    Added get method to get a single inventory item data by id.

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,12 +1,33 @@
 const knex = require("knex")(require("../knexfile"));
 
 exports.index = (_req, res) => {
-  knex("inventory")
+  knex("inventories")
     .then((data) => {
       return res.json(data);
     })
     .catch((err) => {
       res.status(400).send(`Error retrieving Inventories: ${err}`);
+    });
+};
+
+// Gets information for an single inventory item by inventory id.
+exports.singleWarehouse = (req, res) => {
+  knex("inventories")
+    .where({ id: req.params.id })
+    .then((inventories) => {
+      if (inventories.length === 0) {
+        return res.status(404).json({
+          message: `Unable to find inventory with id: ${req.params.id}`,
+        });
+      }
+
+      res.json(inventories[0]);
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        message: "There was an issue with the request",
+        error,
+      });
     });
 };
 
