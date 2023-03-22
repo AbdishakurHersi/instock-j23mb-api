9188678ce6b906ef840fae7964258b3c6a0dc464
GabeEllis <gabeellis830@gmail.com>
2023-03-22 11:43:57 -0400

    Got the put request working with email and phone validation. Still need to verify id is an existing ID.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5050;
+
 app.use(cors());
 app.use(express.json());
 
