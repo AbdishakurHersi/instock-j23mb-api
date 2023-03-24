51720d98e44ad54f378ba009a4c081a15209fc40
Abdi Hersi <abdi_shakur23@hotmail.com>
2023-03-24 14:25:05 -0600

    warehouse controller conponent

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -2,8 +2,9 @@ const knex = require("knex")(require("../knexfile"));
 
 exports.index = (_req, res) => {
   knex("warehouses")
-    .then((data) => {
-      res.status(200).json(data);
+    .select("id")
+    .then((warehouses) => {
+      res.status(200).json(warehouses);
     })
     .catch((err) => res.status(400).send(`Error retrieving Warehouses ${err}`));
 };
