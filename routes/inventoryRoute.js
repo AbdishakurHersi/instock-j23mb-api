6bff475e0561dcdf51964570671a7db79445c1cf
Trevor5008 <trevorwerner@hotmail.com>
2023-03-24 20:17:23 -0400

    Added functionality for inventory item deletion

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const inventoryController = require("../controllers/inventoryController");
 
 router.route("/").get(inventoryController.index);
-router.route("/:id").get(inventoryController.singleInventoryItem);
+router.route("/:id")
+   .get(inventoryController.singleInventoryItem)
+   .delete(inventoryController.deleteInventoryItem);
 
 module.exports = router;
