c1bafadd7b4fbdab41fdb4c79dce1d226ba1fb7f
ggf077 <geraldinegf1@gmail.com>
2023-03-25 23:45:50 -0400

    added initial code for edit inventory item to inventoryRoute and inventoryController

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -64,24 +64,55 @@ exports.warehouseInventories = (req, res) => {
 };
 
 exports.deleteInventoryItem = (req, res) => {
-   // Delete the warehouse with ID matching req.params.id
-   knex("inventories")
-     .where({ id: req.params.id })
-     .del()
-     .then((numItemsDeleted) => {
-       if (numItemsDeleted === 0) {
-         return res.status(404).json({
-           message: `Inventory item with id: ${req.params.id} not found`,
-         });
-       }
- 
-       // 204 - No Content
-       res.sendStatus(204);
-     })
-     .catch((error) => {
-       return res.status(400).json({
-         message: "There was an issue with the request",
-         error,
-       });
-     });
- };
+  // Delete the warehouse with ID matching req.params.id
+  knex("inventories")
+    .where({ id: req.params.id })
+    .del()
+    .then((numItemsDeleted) => {
+      if (numItemsDeleted === 0) {
+        return res.status(404).json({
+          message: `Inventory item with id: ${req.params.id} not found`,
+        });
+      }
+
+      // 204 - No Content
+      res.sendStatus(204);
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        message: "There was an issue with the request",
+        error,
+      });
+    });
+};
+
+// Gets information for an single inventory item by inventory id.
+exports.updateInventoryItem = (req, res) => {
+  knex("inventories")
+    .where({ "inventories.id": req.params.id })
+    .innerJoin("warehouses", "warehouses.id", "inventories.warehouse_id")
+    .select(
+      "inventories.id",
+      "warehouse_name",
+      "item_name",
+      "description",
+      "category",
+      "status",
+      "quantity"
+    )
+    .then((inventories) => {
+      if (inventories.length === 0) {
+        return res.status(404).json({
+          message: `Unable to find inventory with id: ${req.params.id}`,
+        });
+      }
+
+      res.json(inventories[0]);
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        message: "There was an issue with the request",
+        error,
+      });
+    });
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const inventoryController = require("../controllers/inventoryController");
 
 router.route("/").get(inventoryController.index);
-router.route("/:id")
-   .get(inventoryController.singleInventoryItem)
-   .delete(inventoryController.deleteInventoryItem);
+router
+  .route("/:id")
+  .get(inventoryController.singleInventoryItem)
+  .delete(inventoryController.deleteInventoryItem)
+  .put(inventoryController.updateInventoryItem);
 
 module.exports = router;
